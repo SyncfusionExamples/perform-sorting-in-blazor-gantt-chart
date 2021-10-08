345befb63ec80d9b3933221fe535f159d9f7f3b2
Arulraj A <arul.raj47@gmail.com>
2021-10-08 14:07:49 +0530

    Committed the example project.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+
+namespace MyBlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTE0MDU1QDMxMzkyZTMzMmUzMEhGaG53Y0tFUXJIaWlSbDgxNEl4dFRhM2IxSkQ0SWd0eG15MVA1eWs4YXM9");
+            builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
